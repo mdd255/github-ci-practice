78f7e6d22e17dd4b57fd9fc1f12460afba9cdca2
mducdung255 <mducdung255@gmail.com>
2025-08-24 23:48:26 +0700

    chore: update jwt secret

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,4 +1,5 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 import { PassportStrategy } from '@nestjs/passport'
 import { ExtractJwt, Strategy } from 'passport-jwt'
 import { UsersService } from '../../users/users.service'
@@ -6,11 +7,13 @@ import { UsersService } from '../../users/users.service'
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(
+    configService: ConfigService,
     private readonly usersService: UsersService,
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
+      secretOrKey: configService.get('JWT_SECRET'),
     })
   }
 
